d488e4f9c5b48075c12b0e6d5da7eca05b16c09e
thanhluan208 <skthunte33@gmail.com>
2024-01-25 17:11:05 +0700

    update code

--- src/screen/Home/index.jsx
+++ src/screen/Home/index.jsx
@@ -7,7 +7,7 @@ import i18n from "../../../i18n";
 import dayjs from "dayjs";
 import Edit from "../../assets/icons/Edit";
 import Delete from "../../assets/icons/Delete";
-import HomeCard from "./Components/Homecard";
+import HomeCard from "./homecomponents/Homecard";
 
 const data = [
   {
